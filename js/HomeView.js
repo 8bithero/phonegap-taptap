d3b1f7633f605482cf2c0002e96d28f9bbc63607
8bit Hero <pkemanes@gmail.com>
2013-04-11 00:51:11 +0300

    Correction for Part 6

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,5 +1,11 @@
 var HomeView = function(store) {
 
+    this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
+
     this.render = function() {
         this.el.html(HomeView.template());
         return this;
@@ -7,17 +13,12 @@ var HomeView = function(store) {
 
     this.findByName = function() {
         store.findByName($('.search-key').val(), function(employees) {
-            $('employee-list').html(HomeView.liTemplate(employees));
+            $('.employee-list').html(HomeView.liTemplate(employees));
         });
     };
 
-    this.initialize = function() {
-        // Define a div wrapper for the view. The div wrapper is used to attach events.
-        this.el = $('<div/>');
-        this.el.on('keyup', '.search-key', this.findByName);
-    };
-
     this.initialize();
+
 }
 
 HomeView.template = Handlebars.compile($("#home-tpl").html());
